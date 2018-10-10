81613ab323e68b40b3448cd279886a806d5865d5
Shiuan Kai <t105360045@ntut.org.tw>
2018-10-10 23:47:34 +0900

    LAB2 歸位

---
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.user.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
